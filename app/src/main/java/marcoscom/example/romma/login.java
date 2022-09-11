57d8333ddedf5cebd8818a3043cf1f762093357c
decaramarcosdaniel <103468350+decaramarcosdaniel@users.noreply.github.com>
2022-09-11 00:38:38 -0300

    activity registro

--- app/src/main/java/marcoscom/example/romma/login.java
+++ app/src/main/java/marcoscom/example/romma/login.java
@@ -2,13 +2,26 @@ package marcoscom.example.romma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
-
+    Button BtnCREARCUENTA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        BtnCREARCUENTA = findViewById(R.id.BtnCREARCUENTA);
+        BtnCREARCUENTA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, registro.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/marcoscom/example/romma/registro.java
+++ app/src/main/java/marcoscom/example/romma/registro.java
@@ -2,13 +2,26 @@ package marcoscom.example.romma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class registro extends AppCompatActivity {
-
+    Button btnCancelar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro);
+        setContentView(R.layout.activity_registrar);
+
+        btnCancelar = findViewById(R.id.btnCancelar);
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(registro.this, login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
